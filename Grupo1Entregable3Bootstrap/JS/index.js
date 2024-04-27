ba28547010068ecf660b67fb4c336b6d4eddb3e5
Daniel Vargas García <161933152+DaniVG7@users.noreply.github.com>
2024-04-27 01:59:50 -0600

    Add files via upload

---
+++ Grupo1Entregable3Bootstrap/JS/index.js
@@ -0,0 +1,10 @@
+// function printAllDivs(){
+//     console.log(document.querySelectorAll("div"))
+//     }
+//     window.addEventListener("load", printAllDivs);
+//     document.addEventListener("mouseover", function (evento) {
+//       evento.target.classList.add("blackBackground")
+//     })
+//     document.addEventListener("mouseout", function (evento) {
+//       evento.target.classList.remove("blackBackground")
+//     })
